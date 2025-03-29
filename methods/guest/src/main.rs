441c7818035e8cda9013a710e1f2d3211a5e7292
devEMEL <dominichenry882@gmail.com>
2025-03-29 12:21:25 +0100

    guest program done

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,13 +1,28 @@
+
 use risc0_zkvm::guest::env;
+use serde::{Serialize, Deserialize};
+
+const MIN_DONATION_AMOUNT_CONSTANT: i64 =  50000000000000000;  // 0.05 × 10^18 wei = 5 × 10^16 wei, or 50,000,000,000,000,000 wei.
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct Campaign {
+    name: String,
+    description: String,
+    min_donation_amount: u64,
+}
 
 fn main() {
     // TODO: Implement your guest code here
 
     // read the input
-    let input: u32 = env::read();
+    let name = env::read::<i64>();
+    let description = env::read::<i64>();
+    let min_donation_amount = env::read::<i64>();
 
     // TODO: do something with the input
 
+    let result = min_donation_amount >= MIN_DONATION_AMOUNT_CONSTANT;
+
     // write public output to the journal
-    env::commit(&input);
+    env::commit(&result);
 }
